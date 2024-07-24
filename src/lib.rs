f356e98133ffe8e7147a66364ad1f2668a626180
imbolc <me@imbolc.name>
2024-07-24 18:15:19 +0600

    PgExecutor

--- src/lib.rs
+++ src/lib.rs
@@ -225,22 +225,30 @@ pub use traits::{Scheduler, Step};
 pub use worker::Worker;
 
 use chrono::{DateTime, Utc};
-use sqlx::{types::Uuid, PgPool};
+use sqlx::{types::Uuid, PgExecutor};
 use std::time::Duration;
 
 const LOST_CONNECTION_SLEEP: Duration = Duration::from_secs(1);
 
 /// Enqueues the task to be run immediately
-pub async fn enqueue(db: &PgPool, task: &impl Scheduler) -> Result<Uuid> {
+pub async fn enqueue<'e>(db: impl PgExecutor<'e>, task: &impl Scheduler) -> Result<Uuid> {
     task.enqueue(db).await
 }
 
 /// Schedules a task to be run after a specified delay
-pub async fn delay(db: &PgPool, task: &impl Scheduler, delay: Duration) -> Result<Uuid> {
+pub async fn delay<'e>(
+    db: impl PgExecutor<'e>,
+    task: &impl Scheduler,
+    delay: Duration,
+) -> Result<Uuid> {
     task.delay(db, delay).await
 }
 
 /// Schedules a task to run at a specified time in the future
-pub async fn schedule(db: &PgPool, task: &impl Scheduler, at: DateTime<Utc>) -> Result<Uuid> {
+pub async fn schedule<'e>(
+    db: impl PgExecutor<'e>,
+    task: &impl Scheduler,
+    at: DateTime<Utc>,
+) -> Result<Uuid> {
     task.schedule(db, at).await
 }
